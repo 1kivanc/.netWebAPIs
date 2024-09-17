1bd107f47a2a6d3422345528719fce088c2e1222
1kivanc <kivanc0556@gmail.com>
2024-09-17 11:53:54 +0300

    create controller and getAllEmployees

---
+++ EmployeeApi/EmployeeApi/Controllers/EmployeesController.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeApi.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+        public EmployeesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var allEmployees = dbContext.Employees.ToList();
+            return Ok(allEmployees);
+        }
+    }
+}
