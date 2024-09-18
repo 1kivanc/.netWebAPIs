0368e705e951e1e354781f2d1b5bac7a2b4693b6
1kivanc <kivanc0556@gmail.com>
2024-09-18 15:09:07 +0300

    delete employee

--- EmployeeApi/EmployeeApi/Controllers/EmployeesController.cs
+++ EmployeeApi/EmployeeApi/Controllers/EmployeesController.cs
@@ -64,6 +64,19 @@ namespace EmployeeApi.Controllers
             dbContext.SaveChanges();
             return Ok(employee);
         }
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteEmployee(Guid id)
+        {
+            var employee = dbContext.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            dbContext.Employees.Remove(employee);
+            dbContext.SaveChanges();
+            return Ok();
+        }
 
     }
 }
