e021beb42657343c849a2838452f013ec56c2cba
1kivanc <kivanc0556@gmail.com>
2024-09-18 15:02:57 +0300

    Controller add update employee and updateEmployeDto

--- EmployeeApi/EmployeeApi/Controllers/EmployeesController.cs
+++ EmployeeApi/EmployeeApi/Controllers/EmployeesController.cs
@@ -48,5 +48,22 @@ namespace EmployeeApi.Controllers
             }
             return Ok(employee);
         }
+        [HttpPut]
+        [Route("{id:guid}")]
+
+        public IActionResult UpdateEmployee(Guid id,UpdateEmployeeDto updateEmployeeDto)
+        {
+            var employee = dbContext.Employees.Find(id);
+            if (employee == null) {
+                return NotFound();
+            }
+            employee.Name = updateEmployeeDto.Name;
+            employee.Email = updateEmployeeDto.Email;
+            employee.Phone = updateEmployeeDto.Phone;
+            employee.Salary = updateEmployeeDto.Salary;
+            dbContext.SaveChanges();
+            return Ok(employee);
+        }
+
     }
 }
